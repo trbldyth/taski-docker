c96de0f06a1c6e5e19ad4b69e314e3b29ded299b
Michail Baylakov <mbaylakov0@gmail.com>
2023-11-20 16:42:15 +0300

    settings pep8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,13 +61,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': os.getenv('POSTGRES_DB', 'django'),
-        	'USER': os.getenv('POSTGRES_USER', 'django'),
-        	'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        	'HOST': os.getenv('DB_HOST', ''),
-        	'PORT': os.getenv('DB_PORT', 5432)
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
